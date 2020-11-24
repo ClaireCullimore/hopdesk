ed6cea4e4636cf966fb78704d04cdc7a4a33def9
CLaire Cullimore <cascullimore@gmail.com>
2020-11-24 15:54:54 +0000

    bookings pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :workspaces do
-    resources :bookings, only: [:new, :edit, :show]
+    resources :bookings, only: [:new, :edit, :show, :create, :update]
   end
   resources :bookings, only: [:delete]
   resources :users, only: [:show]
