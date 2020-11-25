06f8f2d43814caf039349f8bd2eebc562377af56
farouk bey <w1605195@my.westminster.ac.uk>
2020-11-25 13:03:50 +0100

    workspace reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_155251) do
+ActiveRecord::Schema.define(version: 2020_11_25_114252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2020_11_24_155251) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["workspace_id"], name: "index_bookings_on_workspace_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "workspace_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["workspace_id"], name: "index_reviews_on_workspace_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,5 +62,6 @@ ActiveRecord::Schema.define(version: 2020_11_24_155251) do
 
   add_foreign_key "bookings", "users"
   add_foreign_key "bookings", "workspaces"
+  add_foreign_key "reviews", "workspaces"
   add_foreign_key "workspaces", "users"
 end
