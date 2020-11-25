8a336a4f4e2494c803616ee645f732335cd15b1d
Francesca Cellan-Jones <f.cellanjones@gmail.com>
2020-11-25 17:53:07 +0000

    added flatpickr to show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -24,6 +25,9 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import swal from 'sweetalert';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -31,4 +35,8 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  const options = {enableTime: true, dateFormat: "d-m-Y h:i K"};
+  flatpickr("#booking_start_time", options);
+  flatpickr("#booking_end_time", options);
+  // swal("Hello there!");
 });
