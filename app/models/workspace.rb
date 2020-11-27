56dd242b8a62935a55ebf2956c3c25f684d254ae
farouk bey <w1605195@my.westminster.ac.uk>
2020-11-27 15:17:37 +0100

    map update

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -5,4 +5,6 @@ class Workspace < ApplicationRecord
   has_many_attached :photos
   has_many :workspace_amenities
   has_many :amenities, through: :workspace_amenities
+  geocoded_by :postcode
+  after_validation :geocode, if: :will_save_change_to_postcode?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_121753) do
+ActiveRecord::Schema.define(version: 2020_11_27_110004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,8 @@ ActiveRecord::Schema.define(version: 2020_11_26_121753) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "prices"
     t.text "description"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_workspaces_on_user_id"
   end
 
